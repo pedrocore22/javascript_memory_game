e9091ad1fcca5ebd4ec118ea556082e4a9dbb6e7
pedrocore22 <pedrocore22@itweblearning.com>
2022-06-24 13:55:39 +0200

    array

---
+++ js/main.js
@@ -0,0 +1,54 @@
+const characters = [
+    {
+        name: 'Anna',
+        path: 'img/Anna.png'
+    },
+    {
+        name: 'Campanilla',
+        path: 'img/Campanilla.png'
+    },
+    {
+        name: 'Cenicienta',
+        path: 'img/Cenicienta.png'
+    },
+    {
+        name: 'Elsa',
+        path: 'img/Elsa.png'
+    },
+    {
+        name: 'Flynn',
+        path: 'img/Flynn.png'
+    },
+    {
+        name: 'Genio',
+        path: 'img/Genio.png'
+    },
+    {
+        name: 'HadaMadrina',
+        path: 'img/HadaMadrina.png'
+    },
+    {
+        name: 'Kaa',
+        path: 'img/Kaa.png'
+    },
+    {
+        name: 'Mushu',
+        path: 'img/Mushu.png'
+    },
+    {
+        name: 'Pascal',
+        path: 'img/Pascal.png'
+    },
+    {
+        name: 'Pepito',
+        path: 'img/Pepito.png'
+    },
+    {
+        name: 'Primavera',
+        path: 'img/Primavera.png'
+    },
+    {
+        name: 'Rapunzel',
+        path: 'img/Rapunzel.png'
+    },
+]
